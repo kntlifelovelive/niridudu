3941c20f998a99e6880e8962ce2154d5f6fa086e
kntlifelovelive <kyawnyeinthant2010@gmail.com>
2026-08-12 13:13:54 +0630

    Niri fix someerror

--- config/zsh/config/plugins.zsh
+++ config/zsh/config/plugins.zsh
@@ -1,16 +1,15 @@
 # Oh-My-Zsh
+
 DISABLE_AUTO_UPDATE="true"
+
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME=""
 
-plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
+plugins=(
+  git
+  zsh-syntax-highlighting
+  zsh-autosuggestions
+  zsh-system-clipboard
+)
 
-source $ZSH/oh-my-zsh.sh
-# source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-# source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-# Custom plugins
-source "${ZSH_CUSTOM:-~/.zsh}/plugins/zsh-system-clipboard/zsh-system-clipboard.zsh"
-#git clone https://github.com/kutsan/zsh-system-clipboard ${ZSH_CUSTOM:-~/.zsh}/psource ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source "$ZSH/oh-my-zsh.sh"
